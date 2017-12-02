b45b1b0de7d731fe9241e51e2ea857dc180b3ad2
monzo94 <monzo_94@hotmail.com>
2017-12-02 16:38:04 +0100

    Add day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -2,6 +2,20 @@ def checksum(matrix):
     return sum([max(row) - min(row) for row in matrix])
 
 
+def find_divisible_pair(p):
+    assert len(p) > 1
+
+    checkpoint = 1
+    for i in p:
+        for j in p[checkpoint:]:
+            # Return a tuple (dividend, divisor)
+            if i % j == 0:
+                return i, j
+            if j % i == 0:
+                return j, i
+        checkpoint += 1
+
+
 def main():
     matrix = []
     with open("input") as f:
@@ -9,7 +23,9 @@ def main():
             row = [int(x) for x in line.split('\t')]
             matrix.append(row)
 
-    print("Result:", checksum(matrix))
+    print("Part 1:", checksum(matrix))
+
+    print("Part 2:", sum([fst // snd for fst, snd in [find_divisible_pair(row) for row in matrix]]))
 
 
 if __name__ == "__main__":

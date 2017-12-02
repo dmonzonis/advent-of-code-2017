1f862165eed617ce0b6c341f50b0d4c711013501
monzo94 <monzo_94@hotmail.com>
2017-12-02 11:28:12 +0100

    Modify day1 algorithm so that it works for part 2, and in general for any step

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,12 @@
-def sum_matching(s):
+def sum_matching(s, step):
     assert len(s) > 1
 
     current = s[0]
     result = 0
-    for next in s[1:]:
-        if current == next:
-            result += int(next)
-
-    if s[0] == s[-1]:
-        result += int(s[0])
+    for i in range(len(s)):
+        current = s[i]
+        if current == s[(i + step) % len(s)]:
+            result += int(current)
 
     return result
 
@@ -17,7 +15,7 @@ def main():
     with open("input") as f:
         inp = f.read().strip()
 
-    print("Result:", sum_matching(inp))
+    print("Result:", sum_matching(inp, len(inp) // 2))
 
 
 if __name__ == "__main__":

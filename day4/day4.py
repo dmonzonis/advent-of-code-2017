4b83c40e6406b3839fe6bd8d2b60976c01e452ac
monzo94 <monzo_94@hotmail.com>
2017-12-04 14:12:09 +0100

    Add day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -7,14 +7,26 @@ def has_duplicates(s):
     return False
 
 
+def has_anagram(s):
+    seen = set()
+    for e in s:
+        base = ''.join(sorted(e))
+        if base in seen:
+            return True
+        seen.add(base)
+    return False
+
+
 def main():
     passphrases = []
     with open("input") as f:
         for line in f:
             passphrases.append(line.split())
 
-    valids = [has_duplicates(p) for p in passphrases].count(False)
-    print("Part 1:", valids)
+    valids1 = [has_duplicates(p) for p in passphrases].count(False)
+    valids2 = [has_anagram(p) for p in passphrases].count(False)
+    print("Part 1:", valids1)
+    print("Part 2:", valids2)
 
 
 if __name__ == "__main__":

050b771a135957736822a337d7ee3cc880b1108b
monzo94 <monzo_94@hotmail.com>
2017-12-05 10:40:35 +0100

    Add day 5 part 2

--- day5/day5.py
+++ day5/day5.py
@@ -1,10 +1,14 @@
-def escape_maze(maze):
+def escape_maze(maze, part=1):
     """Escape the maze and return the total steps it took."""
     index = 0
     count = 0
+    maze = maze[:]  # Make a copy so it doesn't modify the original
     while index < len(maze) and index >= 0:
         step = maze[index]
-        maze[index] += 1
+        if part == 2 and step >= 3:
+            maze[index] -= 1
+        else:
+            maze[index] += 1
         index += step
         count += 1
     return count
@@ -15,6 +19,7 @@ def main():
         maze = [int(n) for n in f]
 
     print("Part 1:", escape_maze(maze))
+    print("Part 2:", escape_maze(maze, part=2))
 
 
 if __name__ == "__main__":

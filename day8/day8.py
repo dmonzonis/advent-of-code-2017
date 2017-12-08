3099492294fb984dda50105da5ab5a1beecedd3f
monzo94 <monzo_94@hotmail.com>
2017-12-09 00:19:26 +0100

    Add day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -10,13 +10,14 @@ def parse_instruction(p):
 def run_instructions(instructions):
     """
     Executes all the instructions passed as an argument and returns a dictionary containing
-    the state of all variables after the entire process
+    the state of all variables after the entire process, and the max value ever held
     """
     # Initialize variables to 0. Don't use defaultdict or get since we want info on the untouched
     # variables too, if any
     state = {}
     for variable, _, _ in instructions:
         state[variable] = 0
+    max_value = 0
 
     # Run instructions one by one
     for variable, value, condition in instructions:
@@ -26,8 +27,10 @@ def run_instructions(instructions):
         condition = ' '.join(temp)
         if eval(condition):
             state[variable] += value
+            if state[variable] > max_value:
+                max_value = state[variable]
 
-    return state
+    return state, max_value
 
 
 def main():
@@ -36,8 +39,9 @@ def main():
         for line in f:
             instructions.append(parse_instruction(line))
 
-    state = run_instructions(instructions)
+    state, max_value = run_instructions(instructions)
     print("Part 1:", max(state.values()))
+    print("Part 2:", max_value)
 
 
 if __name__ == "__main__":

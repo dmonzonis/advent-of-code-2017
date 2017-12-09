d5035a74060ba4c0f6381a00094f6640178733d0
monzo94 <monzo_94@hotmail.com>
2017-12-09 22:25:37 +0100

    Add day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -1,37 +1,47 @@
 def skip_garbage(stream, index):
+    """
+    Returns the index after the garbage closing tag, and the number of non-canceled garbage
+    characters
+    """
+    garbage = 0
     # Index points to the first character after the garbage starting point
     while True:
         if stream[index] == '!':
             index += 2
         elif stream[index] == '>':
-            return index + 1
+            return index + 1, garbage
         else:
             index += 1
+            garbage += 1
 
 
 def scan_stream(stream):
-    index, depth, result = 0, 0, 0
+    """Returns the score and the total non-canceled garbage of the stream"""
+    index = depth = score = total_garbage = 0
     while index < len(stream):
         if stream[index] == '{':
             index += 1
             depth += 1
         elif stream[index] == '<':
-            index = skip_garbage(stream, index)
+            index, garbage = skip_garbage(stream, index + 1)
+            total_garbage += garbage
         elif stream[index] == '}':
             index += 1
-            result += depth
+            score += depth
             depth -= 1
         else:
             index += 1
 
-    return result
+    return score, total_garbage
 
 
 def main():
     with open("input") as f:
         inp = f.read()
 
-    print("Part 1:", scan_stream(inp))
+    score, garbage = scan_stream(inp)
+    print("Part 1:", score)
+    print("Part 2:", garbage)
 
 
 if __name__ == "__main__":

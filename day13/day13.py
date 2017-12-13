940f6038f3fe317dfe8591540b76f83f47bb4ff4
monzo94 <monzo_94@hotmail.com>
2017-12-13 20:27:42 +0100

    Greatly optimize code

--- day13/day13.py
+++ day13/day13.py
@@ -1,48 +1,16 @@
-class Firewall:
-    def __init__(self, inp):
-        self.state = {}
-        parsed = parse_input(inp)
-        for depth, max_range in parsed:
-            # Initially, all scanners are at position 0 (top), and start moving down
-            self.state[depth] = {'position': 0, 'range': max_range, 'direction': 1}
-
-    def update(self):
-        """Move scanners on every depth layer one step."""
-        for depth in self.state.keys():
-            # Change direction if needed
-            if (self.state[depth]['position'] == self.state[depth]['range'] - 1 and
-                    self.state[depth]['direction'] == 1):
-                self.state[depth]['direction'] = -1
-            elif self.state[depth]['position'] == 0 and self.state[depth]['direction'] == -1:
-                self.state[depth]['direction'] = 1
-
-            # Move
-            self.state[depth]['position'] += self.state[depth]['direction']
-
-    def check_scanner(self, depth, pos):
-        """Check if there is a scanner at given depth and position."""
-        if depth not in self.state:
-            return False
-        return self.state[depth]['position'] == pos
-
-    def severity(self, depth):
-        """Return the severity of getting caught at a given depth."""
-        return depth * self.state[depth]['range']
-
-
 def parse_input(inp):
-    """Parse input and return a list of tuples containing depth and range."""
-    return [(int(d), int(r)) for d, r in [line.split(': ') for line in inp.splitlines()]]
+    """Parse input and return a dictionary containing range for each depth."""
+    return {int(d): int(r) for d, r in [line.split(': ') for line in inp.splitlines()]}
 
 
 def packet_travel(firewall):
     """Send a packet through a firewall and returns the total severity of the trip."""
     severity = 0
-    final_depth = max(firewall.state.keys())
-    for depth in range(final_depth + 1):
-        if firewall.check_scanner(depth, 0):
-            severity += firewall.severity(depth)
-        firewall.update()
+    max_depth = max(firewall.keys())
+    for depth in range(max_depth + 1):
+        if depth in firewall and depth % (2 * (firewall[depth] - 1)) == 0:
+            # Packet caught
+            severity += depth * firewall[depth]
     return severity
 
 
@@ -50,8 +18,10 @@ def main():
     with open("input") as f:
         inp = f.read().strip()
 
-    firewall = Firewall(inp)
+    firewall = parse_input(inp)
+
     print("Part 1:", packet_travel(firewall))
+    #  print("Part 2:", packet_stealth_delay(firewall))
 
 
 if __name__ == "__main__":

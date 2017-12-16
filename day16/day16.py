c482c67297058baee380a083acbcf0f3bfc049c5
monzo94 <monzo_94@hotmail.com>
2017-12-16 10:49:40 +0100

    Add day 16 part 2

--- day16/day16.py
+++ day16/day16.py
@@ -1,34 +1,63 @@
+from functools import partial
+
+
 def spin(seq, size):
     seq = seq[-size:] + seq[:len(seq) - size]
     return seq
 
 
 def exchange(seq, pos_a, pos_b):
+    seq = seq[:]
     seq[pos_a], seq[pos_b] = seq[pos_b], seq[pos_a]
     return seq
 
 
 def partner(seq, prog_a, prog_b):
+    seq = seq[:]
     pos_a, pos_b = seq.index(prog_a), seq.index(prog_b)
     return exchange(seq, pos_a, pos_b)
 
 
-def dance(seq, moves):
-    """Parse and perform all the moves in the move list."""
-    # Parse each move and its arguments, and execute it
+def parse_moves(moves):
+    """Parse a list of moves and return a list of partials that perform each move."""
+    res = []
     for move in moves:
         if move[0] == 's':
             # Spin
             size = int(move[1:])
-            seq = spin(seq, size)
+            part = partial(spin, size=size)
         elif move[0] == 'x':
             # Exchange
             pos_a, pos_b = [int(pos) for pos in move[1:].split('/')]
-            seq = exchange(seq, pos_a, pos_b)
+            part = partial(exchange, pos_a=pos_a, pos_b=pos_b)
         elif move[0] == 'p':
             # Partner
             prog_a, prog_b = [prog for prog in move[1:].split('/')]
-            seq = partner(seq, prog_a, prog_b)
+            part = partial(partner, prog_a=prog_a, prog_b=prog_b)
+
+        res.append(part)
+    return res
+
+
+def dance(seq, partials):
+    """Parse and perform all the moves in a partial list."""
+    seq = seq[:]
+    # Parse each move and its arguments, and execute it
+    for move in partials:
+        seq = move(seq)
+    return seq
+
+
+def repeat_dance(seq, partials, times=1000000000):
+    """Repeat dance until we're done or we find a cycle, and return the final result."""
+    seen = []
+    for t in range(times):
+        s = ''.join(seq)
+        # If there's a cycle, we don't need to go on
+        if s in seen:
+            return(seen[times % t])
+        seen.append(s)
+        seq = dance(seq, partials)
     return seq
 
 
@@ -38,8 +67,11 @@ def main():
 
     seq = [chr(i) for i in range(ord('a'), ord('p') + 1)]
 
-    after_dance = ''.join(dance(seq, moves))
-    print("Part 1:", after_dance)
+    partials = parse_moves(moves)
+    after_dance = dance(seq, partials)
+    print("Part 1:", ''.join(after_dance))
+
+    print("Part 2:", ''.join(repeat_dance(seq, partials)))
 
 
 if __name__ == "__main__":

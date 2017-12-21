fd516f4953a43a234592e1bfe795a664fcfb21ed
monzo94 <monzo_94@hotmail.com>
2017-12-21 20:58:14 +0100

    Encapsulate system behavior into ParticleSystem class

--- day20/day20.py
+++ day20/day20.py
@@ -1,7 +1,34 @@
+from copy import deepcopy
+
 import numpy as np
 
 
+class ParticleSystem:
+    """Represents a system of particles that move independently in the same space.
+
+    Also saves info about the initial state of the system.
+    """
+
+    def __init__(self, particles):
+        """Generate particle system from a list of particles."""
+        self.system = particles
+        # Save a copy of the initial state of the system for reset
+        self._initial = deepcopy(self.system)
+        self.time = 0
+
+    def update(self, t):
+        """Update the state of the system after after a time t has passed."""
+        for particle in self.system:
+            particle.update(t)
+
+    def reset(self):
+        """Reset the system to the initial state."""
+        self.system = deepcopy(self._initial)
+
+
 class Particle:
+    """Represents a particle that moves with constant acceleration in 3D space."""
+
     def __init__(self, id, pos, vel, acc):
         """Initialize the particle's state using initial conditions."""
         self.id = id
@@ -10,7 +37,7 @@ class Particle:
         self.acc = acc
 
     def update(self, t):
-        """Updates position and velocity after a time t has passed."""
+        """Update position and velocity after a time t has passed."""
         self.pos = self.pos + t * self.vel + self.acc * t * (t + 1) / 2
         self.vel = self.vel + self.acc * t
 
@@ -38,7 +65,7 @@ def parse_initial_conditions(states):
     return vectors
 
 
-def create_particles(initial_conditions):
+def create_particle_system(initial_conditions):
     """Return a list of particles initialized using the initial conditions."""
     particles = []
     id = 0
@@ -47,20 +74,14 @@ def create_particles(initial_conditions):
         particles.append(Particle(id, np.array(pos), np.array(vel), np.array(acc)))
         id += 1
 
-    return particles
+    return ParticleSystem(particles)
 
 
-def update_system(particles, t):
-    """Update the particle system after a time t has passed."""
-    for particle in particles:
-        particle.update(t)
-
-
-def find_closest_to_origin(particles):
+def find_closest_to_origin(system):
     """Find the closest particle to the origin using the Manhattan distance."""
     closest = None
     dist = float('inf')
-    for particle in particles:
+    for particle in system.system:
         particle_dist = particle.manhattan_dist()
         if particle_dist < dist:
             closest = particle
@@ -72,9 +93,9 @@ def main():
     with open("input") as f:
         initial_conditions = parse_initial_conditions(f.read().splitlines())
 
-    particles = create_particles(initial_conditions)
-    update_system(particles, 1000000)
-    print("Part 1:", find_closest_to_origin(particles))
+    particle_system = create_particle_system(initial_conditions)
+    particle_system.update(1000000)
+    print("Part 1:", find_closest_to_origin(particle_system))
 
 
 if __name__ == "__main__":

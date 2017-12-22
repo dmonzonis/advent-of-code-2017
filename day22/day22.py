5babb4988219ab23bf58f6562aefec5e1ccbcb30
monzo94 <monzo_94@hotmail.com>
2017-12-22 16:09:45 +0100

    Add day 22 part 2

--- day22/day22.py
+++ day22/day22.py
@@ -3,13 +3,14 @@ from collections import defaultdict
 
 class Grid:
     def __init__(self, sgrid):
-        self.infected = defaultdict(bool)
+        # 0 -> clean, 1 -> weakened, 2 -> infected, 3 -> flagged
+        self.states = defaultdict(int)
 
         sgrid = sgrid.splitlines()
         for y in range(len(sgrid)):
             for x in range(len(sgrid[y])):
                 if sgrid[y][x] == '#':
-                    self.infected[(x, y)] = True
+                    self.states[(x, y)] = 2
 
         self.origin = (len(sgrid[0]) // 2, len(sgrid) // 2)
 
@@ -30,13 +31,25 @@ class Virus:
     def move(self):
         self.pos = tuple([sum(x) for x in zip(self.pos, self.direction)])
 
-    def infection_step(self):
-        if self.grid.infected[self.pos]:
+    def infection_step(self, flags=False):
+        status = self.grid.states[self.pos]
+        if status == 0:
+            self.turn_left()
+            if not flags:
+                self.total_infections += 1
+        elif status == 1:
+            self.total_infections += 1
+        elif status == 2:
             self.turn_right()
         else:
-            self.turn_left()
-            self.total_infections += 1
-        self.grid.infected[self.pos] = not self.grid.infected[self.pos]
+            self.turn_right()
+            self.turn_right()
+
+        if flags:
+            self.grid.states[self.pos] = (status + 1) % 4
+        else:
+            self.grid.states[self.pos] = (status + 2) % 4
+
         self.move()
 
 
@@ -52,6 +65,13 @@ def main():
 
     print("Part 1:", virus.total_infections)
 
+    grid = Grid(inp)
+    virus = Virus(grid)
+
+    for _ in range(10000000):
+        virus.infection_step(flags=True)
+
+    print("Part 2:", virus.total_infections)
 
 
 if __name__ == "__main__":
